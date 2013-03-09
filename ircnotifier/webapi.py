86a2df09058cacafe0c1ad8af937dc3eff9c98ad
James Mills <prologic@shortcircuit.net.au>
2013-03-10 02:14:16 +1000

    Tidied the postcommit code up

--- ircnotifier/webapi.py
+++ ircnotifier/webapi.py
@@ -14,10 +14,6 @@ from circuits.web import JSONController
 from circuits.net.protocols.irc import PRIVMSG
 
 
-BB_UPDATE_TPL = """\
-{name:s}: 8{user:s} 12{node:s} {message:s} ({files:s})"""
-
-
 class WebAPI(JSONController):
 
     channel = "/api"
@@ -37,49 +33,20 @@ class WebAPI(JSONController):
         payload = loads(kwargs.get("payload", "{}"))
 
         name = payload.get("repository", {}).get("name", "Unknown")
-        user = payload.get("user", "Anonymous")
         commits = payload.get("commits", [])
-        commit = commits and commits[0]
-        message = commit.get("message", "")
-        node = commit.get("node", "")
-        files = commit.get("files", [])
-        files = [file["file"] for file in files]
 
-        data = {
-            "name": name,
-            "user": user,
-            "node": node,
-            "files": files,
-            "message": message
-        }
+        self.fire(
+            PRIVMSG(
+                channel,
+                "{0:d} commit(s) pushed to {1:s}".format(
+                    len(commits), name
+                )
+            ),
+            "bot"
+        )
 
-        pprint(data)
+        for commit in commits:
+            self.fire(PRIVMSG(channel, "{0:s} by {1:s}: {2:s}".format(
+                commit["node"], commit["author"], commit["message"])), "bot")
 
-        self.fire(PRIVMSG(channel, BB_UPDATE_TPL.format(**data)), "bot")
         return {"success": True}
-
-"""
-{u'canon_url': u'https://bitbucket.org',
- u'commits': [{u'author': u'prologic',
-               u'branch': u'default',
-               u'files': [{u'file': u'hello.txt', u'type': u'added'}],
-               u'message': u'Initial Commit',
-               u'node': u'e8ce5979e129',
-               u'parents': [],
-               u'raw_author': u'prologic',
-               u'raw_node': u'e8ce5979e129cdbfa1f3c40f4547b8bfa8203261',
-               u'revision': 0,
-               u'size': -1,
-               u'timestamp': u'2013-03-09 15:56:08',
-               u'utctimestamp': u'2013-03-09 14:56:08+00:00'}],
- u'repository': {u'absolute_url': u'/prologic/test/',
-                 u'fork': False,
-                 u'is_private': False,
-                 u'name': u'test',
-                 u'owner': u'prologic',
-                 u'scm': u'hg',
-                 u'slug': u'test',
-                 u'website': u''},
- u'truncated': False,
- u'user': u'prologic'}
-"""
